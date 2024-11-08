e826876fb50f92a9dfe514f14a616f4d1b58db14
julpermaz <julpermaz@alu.edu.gva.es>
2024-11-08 10:08:06 +0100

    long1

---
+++ src/main/palabras_long.java
@@ -0,0 +1,24 @@
+package main;
+
+public class palabras_long {
+
+    public static int cuantas_p (String[] palabras) {
+        int contador = 0;
+        int cuantas = 3;
+        for (int i = 0; i < palabras.length; i++) {
+            if (palabras[i].length() == cuantas) {
+                contador ++;
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        String cadena = "Chico que estas haciendo";
+        int cuantas = 3;
+        String[] palabras = cadena.split(" ");
+
+        int res = cuantas_p(palabras);
+        System.out.print("Hay " + res + " palabras que tienen " + cuantas + " letras");
+    }
+}
